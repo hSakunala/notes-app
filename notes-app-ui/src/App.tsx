26a4dce8b77d4963840954a89055e7294b98bcfe
Hsakunala <hrishikeshsakunala@gmail.com>
2025-03-03 13:30:43 -0500

    Second Commit

---
+++ notes-app-ui/src/App.tsx
@@ -0,0 +1,32 @@
+import "./App.css"
+
+const App = () => {
+    return (
+        <div className="app-container">
+            <form className="note-form">
+                <input
+                    placeholder="title"
+                    required
+                ></input>
+                <textarea
+                    placeholder="Content"
+                    rows={10}
+                    required
+                ></textarea>
+                <button type="submit">
+                    Add Note
+                </button>
+            </form>
+            <div className="notes-grid">
+                <div className="note_item">
+                    <div className="notes-header">
+                        <button>x</button>
+                    </div>
+                    <h2>Note Title</h2>
+                    <p>Note Content</p>
+                </div>
+            </div>
+        </div>);
+};
+
+export default App;
